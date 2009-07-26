0e7d7617c510f369c772d9ae74d163e6fb49eb79
Alcides Fonseca <me@alcidesfonseca.com>
2009-07-26 23:18:42 +0100

    Removed unpublished pages

--- wiki/urls.py
+++ wiki/urls.py
@@ -11,5 +11,5 @@ feeds = {
 urlpatterns = patterns('',
 	(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 	(r'^(?P<slug>\S+)$', 'django.views.generic.list_detail.object_detail',{ 'queryset': Page.objects.all() }),
-    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Page.objects.all(), 'paginate_by': 20 }),
+    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), 'paginate_by': 20 }),
 )
