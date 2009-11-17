2a8e7563486d0d209efe06b3b63738fe051c0744
Alcides Fonseca <me@alcidesfonseca.com>
2009-11-18 00:38:48 +0100

    Added request parameter

--- settings.py
+++ settings.py
@@ -42,6 +42,14 @@ INSTALLED_APPS = (
         'debug_toolbar',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	"django.core.context_processors.request",
+	"django.core.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media"
+)
+
 TEMPLATE_DIRS = (
 	relative('templates'),
 )
