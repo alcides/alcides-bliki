fadbab40256fcbbf83fda50fb8cfe8e5e0a1e68f
Alcides Fonseca <me@alcidesfonseca.com>
2010-01-09 10:49:58 +0100

    HTML5 and removed JS

--- public/media/js/wiki.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-	$$('sup.footnote a').each(function(i) {
-		i.onclick = function() {
-			field = this.href.split("#")[1];
-			new Effect.Highlight(field, {duration: 2});
-		};
-	});
-}
