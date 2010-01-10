b3f381a650ef457939d0499fa3c453c9774fb1aa
Alcides Fonseca <me@alcidesfonseca.com>
2010-01-10 18:08:35 +0100

    Fixed page insert in history mode

--- wiki/signals.py
+++ wiki/signals.py
@@ -4,10 +4,13 @@ from wiki.models import Page, PageVersion
 
 def pre_save(instance, **kwargs):
   if kwargs["sender"] == Page:
-    original = Page.objects.get(id=instance.id)
-    if instance.text != original.text:
-      if PageVersion.objects.filter(page=original).count() > 0:
-        version = PageVersion.objects.filter(page=original).aggregate(Max('version'))['version__max'] + 1
-      else:
-        version = 1
-      PageVersion.objects.create(page=original, text=original.text, version=version)
+    try:
+      original = Page.objects.get(id=instance.id)
+      if instance.text != original.text:
+        if PageVersion.objects.filter(page=original).count() > 0:
+          version = PageVersion.objects.filter(page=original).aggregate(Max('version'))['version__max'] + 1
+        else:
+          version = 1
+        PageVersion.objects.create(page=original, text=original.text, version=version)
+    except:
+      pass
