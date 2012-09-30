6d10f1cdef1dc806f14ebdbef023742d17e6e1fa
Alcides Fonseca <me@alcidesfonseca.com>
2012-10-01 00:19:48 +0100

    Feeds upgraded to 1.4

--- wiki/urls.py
+++ wiki/urls.py
@@ -2,14 +2,10 @@ from django.conf.urls.defaults import *
 from models import Page
 from feeds import English, Portuguese, All
 
-feeds = {
-    'en': English,
-    'pt': Portuguese,
-	'all': All,
-}
-
 urlpatterns = patterns('',
-	(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+	(r'^feeds/all/$', All),
+	(r'^feeds/pt/$', Portuguese),
+	(r'^feeds/en/$', English),
 	(r'^(?P<slug>\S+)/$', 'django.views.generic.list_detail.object_detail',{ 'queryset': Page.objects.all() }),
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), 'paginate_by': 20 }),
 )
