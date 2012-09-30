a8bf9e354faa6eb6caf5785ff1f0bb720b186568
Alcides Fonseca <me@alcidesfonseca.com>
2012-10-01 00:23:39 +0100

    Bug fixed with constructor of feeds.

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,9 +3,9 @@ from models import Page
 from feeds import English, Portuguese, All
 
 urlpatterns = patterns('',
-	(r'^feeds/all/$', All),
-	(r'^feeds/pt/$', Portuguese),
-	(r'^feeds/en/$', English),
+	(r'^feeds/all/$', All()),
+	(r'^feeds/pt/$', Portuguese()),
+	(r'^feeds/en/$', English()),
 	(r'^(?P<slug>\S+)/$', 'django.views.generic.list_detail.object_detail',{ 'queryset': Page.objects.all() }),
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), 'paginate_by': 20 }),
 )
