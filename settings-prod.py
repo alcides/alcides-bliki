a13e61148a13512416d6b608490c9b99577a7de5
Alcides Fonseca <me@alcidesfonseca.com>
2012-09-30 22:10:02 +0100

    Upgraded production to 1.4

--- settings-prod.py
+++ settings-prod.py
@@ -3,12 +3,12 @@ from settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = relative('db/prod.db') # Or path to database file if using sqlite3.
-DATABASE_USER = ''
-DATABASE_PASSWORD = ''
-DATABASE_HOST = ''             # empty -> localhost
-DATABASE_PORT = ''             # empty -> default
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': relative('db/prod.db')
+    }
+}
 
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda x: DEBUG,
