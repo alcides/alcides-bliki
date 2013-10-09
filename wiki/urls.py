12da714f2edaccbab68335a704b1175382689416
Alcides Fonseca <me@alcidesfonseca.com>
2013-10-09 11:52:56 +0100

    Migrated function to class-based generic views.

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import *
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
 from models import Page
 from feeds import English, Portuguese, All
+
 
 urlpatterns = patterns('',
 	(r'^feeds/all/$', All()),
 	(r'^feeds/pt/$', Portuguese()),
 	(r'^feeds/en/$', English()),
-	(r'^(?P<slug>\S+)/$', 'django.views.generic.list_detail.object_detail',{ 'queryset': Page.objects.all() }),
-    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), 'paginate_by': 20 }),
+	(r'^(?P<slug>\S+)/$', DetailView.as_view(queryset=Page.objects.all())),
+    (r'^$', ListView.as_view(queryset= Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), paginate_by=20 )),
 )
