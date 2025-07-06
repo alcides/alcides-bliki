e5374f9d1e0ee736337b1dc9de50408e4a5a07bd
Alcides Fonseca <me@alcidesfonseca.com>
2025-07-06 17:10:32 +0100

    Feeds do not include articles in the future.

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,3 +1,4 @@
+import datetime
 from django.conf import settings
 from django.urls import path, re_path, include, register_converter
 from django.conf.urls.static import static
@@ -27,6 +28,6 @@ urlpatterns = [
     path('feeds/all/', All()),
 	path('feeds/pt/', Portuguese()),
 	path('feeds/en/', English()),
-    path('', ListView.as_view(queryset= Page.objects.filter(pubdate__isnull=False).order_by('-pubdate'), paginate_by=20 )),
+    path('', ListView.as_view(queryset= Page.objects.filter(pubdate__isnull=False).exclude(pub_date__gte=datetime.date.today()).order_by('-pubdate'), paginate_by=20 )),
     re_path('^(?!(admin|media|static))(?P<slug>[\w/\-_]*)/$', DetailView.as_view(queryset=Page.objects.all()), name="page_detail"),
 ]
